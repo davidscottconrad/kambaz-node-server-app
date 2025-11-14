26154a9ef131ec4ea2553e9ff04dfb430f134c0b
David Conrad <david.scott.conrad@gmail.com>
2025-11-13 19:42:48 -0800

    up to working with objects

---
+++ Lab5/WorkingWithObjects.js
@@ -0,0 +1,23 @@
+const assignment = {
+    id: 1, title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10", completed: false, score: 0,
+};
+const setAssignmentTitle = (req, res) => {
+    const { newTitle } = req.params;
+    assignment.title = newTitle;
+    res.json(assignment);
+};
+
+export default function WorkingWithObjects(app) {
+    const getAssignment = (req, res) => {
+        res.json(assignment);
+    };
+    const getAssignmentTitle = (req, res) => {
+        res.json(assignment.title);
+    };
+    app.get("/lab5/assignment/title", getAssignmentTitle);
+    app.get("/lab5/assignment", getAssignment);
+
+    app.get("/lab5/assignment/title/:newTitle", setAssignmentTitle);
+};
