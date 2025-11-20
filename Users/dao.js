92abbf40051d4cf5e872abafbb13d2e7f515059a
David Conrad <david.scott.conrad@gmail.com>
2025-11-20 12:42:46 -0800

    find users by role

--- Users/dao.js
+++ Users/dao.js
@@ -4,10 +4,11 @@ export default function UsersDao() {
     const createUser = () => { } // implemented later
     const findAllUsers = () => model.find();
     console.log("Users in DAO:", findAllUsers());
+    const findUsersByRole = (role) => model.find({ role: role })
     const findUserById = (userId) => model.findById(userId);
     const findUserByUsername = (username) => model.findOne({ username: username });
     const findUserByCredentials = (username, password) => model.findOne({ username, password });
     const updateUser = (userId, user) => model.updateOne({ _id: userId }, { $set: user });
     const deleteUser = (userId) => model.deleteOne({ _id: userId });
-    return { createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser };
+    return { findUsersByRole, createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser };
 }
