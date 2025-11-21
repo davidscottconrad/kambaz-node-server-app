b856da1875f01fdef1aec507cf9b2044e7a31a3e
David Conrad <david.scott.conrad@gmail.com>
2025-11-21 10:14:29 -0800

    fix?

--- index.js
+++ index.js
@@ -14,14 +14,13 @@ import mongoose from "mongoose";
 const app = express();
 
 // trust proxy only in production
-if (process.env.SERVER_ENV !== "development") {
-    app.set('trust proxy', 1);
-}
+// if (process.env.NODE_ENV !== "development") {
+app.set('trust proxy', 1);
+// }
 
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
 mongoose.connect(CONNECTION_STRING);
 
-// CORS first: allow only your frontend
 app.use(
     cors({
         credentials: true,
@@ -29,21 +28,20 @@ app.use(
     })
 );
 
-// Session (after CORS)
 const sessionOptions = {
     secret: process.env.SESSION_SECRET || "kambaz",
     resave: false,
     saveUninitialized: false,
 };
 
-if (process.env.SERVER_ENV !== "development") {
-    sessionOptions.proxy = true;
-    sessionOptions.cookie = {
-        sameSite: "none",
-        secure: true,
-        domain: process.env.SERVER_URL,
-    };
-}
+// if (process.env.NODE_ENV !== "development") {
+sessionOptions.proxy = true;
+sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+};
+// }
 
 
 app.use(session(sessionOptions));
