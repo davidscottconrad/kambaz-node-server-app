98aa96c62a3dbddc13a4848a921b568bfaa46345
David Conrad <david.scott.conrad@gmail.com>
2025-11-21 08:53:48 -0800

    fix cookie issue

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ import "dotenv/config";
 import session from "express-session";
 import mongoose from "mongoose";
 const app = express();
+
+if (process.env.SERVER_ENV !== "development") {
+    app.set('trust proxy', 1);
+}
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 
